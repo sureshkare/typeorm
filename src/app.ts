ab4a1c26af29d6d8b4382be8e83e3ffcd1736acc
rajukare <bangarrajuk23@gmail.com>
2023-02-16 17:39:31 +0530

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
+import 'reflect-metadata';
 import express from 'express';
+import {DataSource} from 'typeorm';
+import {User} from './entities/User';
 
 const app = express();
-
 app.use(express.json());
 const port = 3005;
 
@@ -9,6 +11,24 @@ app.get('/', (req,res) => {
     console.log("Hello from express");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on ${port}`)
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "suresh",
+    database: "typeorm_db",
+    entities: [User],
+    synchronize: true,
+    logging: true,
 });
+
+AppDataSource.initialize().then(() => {
+    console.log("Database is connected successfully");
+    app.listen(port, () => {
+        console.log(`Server is running on ${port}`)
+    });
+}).catch((err) => {
+    console.log("Database is not connected",err);
+});
+
